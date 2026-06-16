36f7adaf1fe9de7a8eb5876be1cc3b4c3a804e8d
Jeet Patel <pateljp6153@gmail.com>
2026-06-16 12:48:04 -0600

    Create properties.js
    This file creates the properties table and use it to GET, POST and DELETE data accordingly.

---
+++ backend/routes/properties.js
@@ -0,0 +1,80 @@
+// Took help from the code provided by the professor for in-class example.
+// Took help from W3Schools "https://www.w3schools.com/nodejs/nodejs_express.asp" to understand and use
+// "const router = express.Router();" and other functions as well.
+// express.Router(); helps to route the individual js functions file to the server.
+
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+
+// Creating Properties Table.
+db.run(`CREATE TABLE IF NOT EXISTS properties (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        ownerId INTEGER NOT NULL,
+        address TEXT NOT NULL,
+        neighborhood TEXT NOT NULL,
+        squareFeet INTEGER NOT NULL,
+        parking TEXT NOT NULL,
+        pTransit TEXT NOT NULL)` , function (err) {
+    if (err) {
+        console.log('Error creating table:', err.message);
+    } else {
+        console.log('Properties table ready!');
+    }
+});
+
+// Geting all the properties that belongs to the particular owner.
+router.get("/properties", (req, res) => {
+    const ownerId = req.query.ownerId;
+
+    db.all("SELECT * FROM properties WHERE ownerId = " + ownerId, (err, rows) => {
+        if (err) {
+            return res.status(500).json({
+                error: err.message
+            });
+        }
+        res.json(rows);
+    });
+});
+
+// Posting all the new properties to the database.
+router.post("/properties", (req, res) => {
+    const { ownerId, address, neighborhood, squareFeet, parking, pTransit } = req.body;
+
+    db.run(`
+        INSERT INTO properties 
+        (ownerId, address, neighborhood, squareFeet, parking, pTransit)
+        VALUES (?, ?, ?, ?, ?, ?)`,
+        [ownerId, address, neighborhood, squareFeet, parking, pTransit],
+        function (err) {
+            if (err) {
+                return res.status(500).json({
+                    error: err.message
+                });
+            }
+            res.json({
+                message: "Property added successfully",
+                id: this.lastID
+            });
+        }
+    );
+});
+
+
+// Delete the property according to the id.
+router.delete("/properties/:id", (req, res) => {
+    const id = req.params.id;
+
+    db.run("DELETE FROM properties WHERE id = " + id, function (err) {
+        if (err) {
+            return res.status(500).json({
+                error: err.message
+            });
+        }
+        res.json({
+            message: "Property deleted successfully"
+        });
+    });
+});
+
+module.exports = router;
