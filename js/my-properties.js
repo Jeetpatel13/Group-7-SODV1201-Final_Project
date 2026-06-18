d8240b89b4ae70d2935a4b7027814a197e3aae6e
Jeet Patel <pateljp6153@gmail.com>
2026-06-17 18:27:57 -0600

    Update my-properties.js

--- js/my-properties.js
+++ js/my-properties.js
@@ -32,39 +32,48 @@ function checkLogin() {
 // Used class notes and code given by proffesor in term 1 and 2
 function loadPage() {
 
-    let myProperties = [];
-    for (let i = 0; i < properties.length; i++) {
-        if (properties[i].ownerId === currentUser.id) {
-            myProperties.push(properties[i]);
-        };
-    };
+    // let myProperties = [];
+    // for (let i = 0; i < properties.length; i++) {
+    //     if (properties[i].ownerId === currentUser.id) {
+    //         myProperties.push(properties[i]);
+    //     };
+    // };
 
-    let myWorkspaces = [];
-    for (let i = 0; i < workspaces.length; i++) {
-        for (let j = 0; j < myProperties.length; j++) {
-            if (workspaces[i].propertyId === myProperties[j].id) {
-                myWorkspaces.push(workspaces[i]);
+    // let myWorkspaces = [];
+    // for (let i = 0; i < workspaces.length; i++) {
+    //     for (let j = 0; j < myProperties.length; j++) {
+    //         if (workspaces[i].propertyId === myProperties[j].id) {
+    //             myWorkspaces.push(workspaces[i]);
 
-            };
-        };
-    };
+    //         };
+    //     };
+    // };
+
+    // $("#numProperties").text(myProperties.length);
+    // $("#numWorkspace").text(myWorkspaces.length);
+    // $("#numListing").text(myWorkspaces.length);
+
+
+
+    // if (myWorkspaces.length > 0) {
+    //     let earliest = new Date(myWorkspaces[0].availabilityDate);
+    //     for (let i = 1; i < myWorkspaces.length; i++) {
+    //         let date = new Date(myWorkspaces[i].availabilityDate);
+    //         if (date < earliest) {
+    //             earliest = date;
+    //         }
+    //     }
+    //     $("#date").text(earliest.toLocaleDateString());
+    // }
+
+    // displayTable(myProperties);
+
+    const response = await fetch("http://localhost:3000/properties?ownerId=" + currentUser.id);
+    const myProperties = await response.json();
 
     $("#numProperties").text(myProperties.length);
-    $("#numWorkspace").text(myWorkspaces.length);
-    $("#numListing").text(myWorkspaces.length);
-
-
-
-    if (myWorkspaces.length > 0) {
-        let earliest = new Date(myWorkspaces[0].availabilityDate);
-        for (let i = 1; i < myWorkspaces.length; i++) {
-            let date = new Date(myWorkspaces[i].availabilityDate);
-            if (date < earliest) {
-                earliest = date;
-            }
-        }
-        $("#date").text(earliest.toLocaleDateString());
-    }
+    $("#numWorkspace").text(0);
+    $("#numListing").text(myProperties.length);
 
     displayTable(myProperties);
 
@@ -105,14 +114,27 @@ function displayTable(myProperties) {
 
 function deleteProperty(id) {
 
-    properties = properties.filter(function (element) {
-        return element.id !== id;
+    // properties = properties.filter(function (element) {
+    //     return element.id !== id;
+    // });
+
+    // workspaces = workspaces.filter(function (element) {
+    //     return element.propertyId !== id;
+    // });
+
+    // $("#statusMessage").text("Property deleted successfully!");
+    // loadPage();
+
+    const response = await fetch("http://localhost:3000/properties/" + id, {
+        method: "DELETE"
     });
 
-    workspaces = workspaces.filter(function (element) {
-        return element.propertyId !== id;
-    });
+    const data = await response.json();
 
-    $("#statusMessage").text("Property deleted successfully!");
-    loadPage();
+    if (data.message) {
+        $("#statusMessage").text("Property deleted successfully!");
+        loadPage();
+    } else {
+        $("#statusMessage").text("Error: " + data.error);
+    }
 }
