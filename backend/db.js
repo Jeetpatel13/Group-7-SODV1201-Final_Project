c35eff6670dc1b746b1880b7e8276d9f00a01d94
Gurleen Kaur <gurleenkaur97520@gmail.com>
2026-06-19 16:57:16 -0600

    Fix database file path in db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@
 // Also used the code given by the professor for in-class example.
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./database.db", (err) => {
+const db = new sqlite3.Database("database.db", (err) => {
     if (err) {
         console.log(err.message);
     }
